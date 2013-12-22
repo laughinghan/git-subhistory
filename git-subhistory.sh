96ab931807f504d3a69d1154214384504508db66
Han <laughinghan@gmail.com>
2013-12-21 17:06:50 -0700

    Skeleton git-subhistory.sh: parse opts, def log fns
    Based on git-subtree.sh, git-submodule.sh, and git-bisect.sh, except
    following the Git Coding Guidelines (which none of those fully do).
    
    The clever, extensible string substitution-based subcommand dispatch in
    Main is from git-subtree.sh/git-submodule.sh (presumably the former got
    it from the latter), but with the git-bisect.sh naming scheme, which I
    like better.

---
+++ git-subhistory.sh
@@ -0,0 +1,103 @@
+#!/bin/sh
+# http://github.com/laughinghan/git-subhistory
+
+# util fn (at the top 'cos used in options parsing)
+die () {
+	echo "$@" >&2
+	exit 1
+}
+
+######################
+# Options Parsing
+#   >:( so many lines
+
+# if zero args, default to -h
+test $# = 0 && set -- -h
+
+OPTS_SPEC="\
+git-subhistory split <subproj-path> (-b | -B) <subproj-branch>
+git-subhistory merge <subproj-path> <subproj-branch>
+--
+q,quiet         be quiet
+v,verbose       be verbose
+h               show the help
+
+ options for 'split':
+b=              create a new branch for the split-out commit history
+B=              like -b but force creation"
+
+eval "$(echo "$OPTS_SPEC" | git rev-parse --parseopt -- "$@" || echo exit $?)"
+# ^ this is actually what you're supposed to do, see `git rev-parse --help`
+
+quiet="$GIT_QUIET"
+verbose=
+newbranch=
+force_newbranch=
+
+while test $# != 0
+do
+	case "$1" in
+	-q|--quiet) quiet=1 ;;
+	--no-quiet) quiet= ;;
+	-v|--verbose) verbose=1 ;;
+	--no-verbose) verbose= ;;
+	-b|-B)
+		test "$1" = "-B" && force_newbranch=1
+		shift
+		newbranch="$1"
+		test "$newbranch" || die "Branch name must be nonempty"
+	;;
+	--) break ;;
+	esac
+	shift
+done
+shift
+
+##############
+# Logging Fns
+
+if test "$quiet"
+then
+	say () {
+		:
+	}
+else
+	say () {
+		echo "$@" >&2
+	}
+fi
+
+if test "$verbose"
+then
+	elaborate () {
+		echo "$@" >&2
+	}
+else
+	elaborate () {
+		:
+	}
+fi
+
+##############
+# Subcommands
+
+subhistory_split () {
+	die "fatal: '$subcommand' not yet implemented"
+}
+
+subhistory_merge () {
+	die "fatal: '$subcommand' not yet implemented"
+}
+
+#######
+# Main
+
+subcommand="$1"
+shift
+
+case "$subcommand" in
+	split|merge) ;;
+	*) die "fatal: unknown subcommand '$subcommand'" ;;
+esac
+
+"subhistory_$subcommand" "$@"
